7aebe4bbab5522c73ca678d948c6d8a310ee5c08
mhmpour <mhmpour@gmail.com>
2017-04-27 03:24:34 -0700

    Update BuildVars.java

--- app/src/main/java/org/telegram/messenger/BuildVars.java
+++ app/src/main/java/org/telegram/messenger/BuildVars.java
@@ -12,11 +12,11 @@ public class BuildVars {
     public static boolean DEBUG_VERSION = false;
     public static int BUILD_VERSION = 0x12bac;
     public static String BUILD_VERSION_STRING = "3.13";
-    public static int APP_ID = 52911; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
-    public static String APP_HASH = "127fcd6f56eb87031d7fd2b3c4d900a0"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
-    public static String HOCKEY_APP_HASH = "3e5111b276824839b8147f673a74d1e0";
-    public static String HOCKEY_APP_HASH_DEBUG_SECRET= "3e5111b276824839b8147f673a74d1e0";
-    public static String HOCKEY_APP_HASH_DEBUG = "3e5111b276824839b8147f673a74d1e0";
+    public static int APP_ID = ; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
+    public static String APP_HASH = ""; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+    public static String HOCKEY_APP_HASH = ""; //add your HOCKEY_APP_HASH
+    public static String HOCKEY_APP_HASH_DEBUG_SECRET= ""; //add your HOCKEY_APP_HASH_DEBUG_SECRET
+    public static String HOCKEY_APP_HASH_DEBUG = ""; //add your HOCKEY_APP_HASH_DEBUG
     public static String GCM_SENDER_ID = "760348033672";
     public static String SEND_LOGS_EMAIL = "mhmpour@gmail.com";
     public static String BING_SEARCH_KEY = ""; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
